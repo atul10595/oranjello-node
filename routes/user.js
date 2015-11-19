2473545cb883f2e95656f9aef110349ba9b5b2eb
Atul Jain <atul13025@iiitd.ac.in>
2015-11-19 19:00:30 +0530

    bleh.

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,5 @@
+// Route for GET and POST related to User
+
 var express = require('express');
 var router = express.Router();
 
@@ -6,9 +8,12 @@ var async = require('async');
 
 
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* POST users listing. */
+router.post('/api/update', function(req, res, next) {
+  
+  var userFbId = req.body.userid; // make sure that android has key-value pair as {"userid":fbId}
+
+  // res.send('respond with a resource');
 });
 
 module.exports = router;
@@ -20,5 +25,4 @@ module.exports = router;
 
 
 /********** extras  */
-
 function pop(str){ console.log("\n=====================[ "+str+" ]=======================\n")}
