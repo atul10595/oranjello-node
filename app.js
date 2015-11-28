5a197eb304d547dc60fa9701491364921fcf6140
Atul Jain <atul13025@iiitd.ac.in>
2015-11-29 02:09:03 +0530

    port 80

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var port = process.env.PORT || 3000; //3000 for development and 80 for production
+var port = process.env.PORT || 80; //3000 for development and 80 for production
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
